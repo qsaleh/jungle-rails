151bdc0a7967f134704eb0f3d74955f7556a520c
qsaleh <qmsaleh@gmail.com>
2020-03-05 00:42:22 +0000

    added route to About us

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'about' => 'about#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
